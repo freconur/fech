dfe48ce163861a6f93c67e5e98900ded71eb5799
fredocev <freconur@mail.com>
2022-11-04 15:48:51 -0500

    ux en contact me

---
+++ src/email/index.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane as message } from "@fortawesome/free-solid-svg-icons";
+import "./email.css";
+import swal from "sweetalert";
+const Email = () => {
+  const formulario = {
+    name: "",
+    mail: "",
+    message: "",
+  };
+  const [value, setValue] = useState(formulario);
+  const onChangeValue = (e) => {
+    if (e.target.name === "user_name") {
+      setValue({
+        ...value,
+        name: e.target.value,
+      });
+    } else if (e.target.name === "user_mail") {
+      setValue({
+        ...value,
+        mail: e.target.value,
+      });
+    } else if (e.target.name === "user_message") {
+      setValue({
+        ...value,
+        message: e.target.value,
+      });
+    }
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    for (const property in value) {
+      if (value[property] === "") {
+        swal({
+          title: "Por favor llena todos los campos",
+          icon: "warning",
+        });
+        break;
+      }
+      emailjs
+        .sendForm(
+          "service_uo3l6tj",
+          "template_ztl700j",
+          e.target,
+          "zuJsKnKK4-oI0q0S8"
+        )
+        .then((response) => {
+          if (response.status === 200) {
+            swal({
+              title: "Acabas de enviarle un mensaje a Franco",
+              icon: "success",
+            });
+          }
+        })
+        .catch((error) => {
+          if (error) {
+            swal({
+              title: "Parece que algo salio mal, intentalo nuevamente",
+              icon: "error",
+            });
+          }
+        });
+      e.target.reset();
+      setValue(formulario);
+    }
+  };
+
+  return (
+    <div className="contact">
+      <h2>contactame</h2>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>nombre</label>
+          <br />
+          <input
+            onChange={onChangeValue}
+            name="user_name"
+            type="text"
+            placeholder="nombre"
+          />
+        </div>
+        <div className="form-group">
+          <label>correo</label>
+          <br />
+          <input
+            onChange={onChangeValue}
+            name="user_mail"
+            type="mail"
+            placeholder="correo"
+          />
+        </div>
+        <div className="form-group">
+          <label>mensaje</label>
+          <br />
+          <textarea
+            onChange={onChangeValue}
+            name="user_message"
+            type="text"
+            placeholder="mensaje"
+          />
+        </div>
+        <button>
+          <FontAwesomeIcon className="icon_message" icon={message} />
+          enviar
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export { Email };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Header } from '../../components/header'
 import { Navbar } from '../../components/navbar'
 import { Projects } from '../../components/Projects'
+import { Email } from '../../email'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar />
         <Header/>
         <Projects />
+        <Email/>
     </div>
   )
 }
